f527b4744b97da93e0a7d2e72d776fc2a592c828
ANMOL KUMAR <anmolkumar10@gmail.com>
2015-09-23 06:34:01 +0530

    last

--- WebQueue/Properties/AssemblyInfo.cs
+++ WebQueue/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebQueue")]
+[assembly: AssemblyTitle("QueueWebApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebQueue")]
+[assembly: AssemblyProduct("QueueWebApp")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7c16d31e-ba26-4866-a342-78528c1eb6c5")]
+[assembly: Guid("f3801ba5-3d6f-4673-a60d-57b717ebd1b7")]
 
 // Version information for an assembly consists of the following four values:
 //
